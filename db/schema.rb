6956bb65956dad295702a10a9953134e9c486ba2
Al Zakharov <azakharov@sibinet.com>
2016-11-21 03:57:34 +0700

    minor schema cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -149,13 +149,6 @@ ActiveRecord::Schema.define(version: 20161120181521) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "roles", force: :cascade do |t|
     t.string   "name",          limit: 255
     t.integer  "resource_id"
